243af5225b4680a5164d1d603e027e000abae79d
securityscience <138095251+securityscience@users.noreply.github.com>
2023-12-04 20:31:33 -0500

    Add files via upload

---
+++ Reports/index.php
@@ -0,0 +1,75 @@
+<!-- -------------------------------------------- -->
+<!--          Sec-Sci AutoPT | 2018-2023          -->
+<!-- -------------------------------------------- -->
+<!-- Site:      www.security-science.com          -->
+<!-- Email:     RnD@security-science.com          -->
+<!-- Creator:   Arnel C. Reyes                    -->
+<!-- @license:  GNU GPL 3.0                       -->
+<!-- @copyright (C) 2018 WWW.SECURITY-SCIENCE.COM -->
+<!-- -------------------------------------------- -->
+
+<html>
+<head>
+<meta name="referrer" content="no-referrer">
+<title>Sec-Sci AutoPT Reports</title>
+<style>
+  table, th, td { border: 1px solid black; border-collapse: collapse; font-family: Arial; font-size: 12px}
+  th, td { padding: 3px; }
+  body { font-family: Arial; color: #404042; background: #dedede; }
+  .frmContainer label { font-size: 12px; font-weight: 500; }
+  a:link, a:visited { text-decoration: none; transform: 0.3s; }
+  a:hover, a:active { color: #e24920; text-decoration: underline; 
+</style>
+
+</head>
+<body>
+
+<script>
+  // Function to create and submit the form
+  function sendParametersToIframe() {
+    var iframeUrl = "lreports.php";
+    var iframe = document.getElementById("lreportView");
+    var projectName = getUrlParameter("projectName");
+
+    // Create a form element
+    var form = document.createElement("form");
+    form.action = iframeUrl;
+    form.method = "post";
+    form.target = "lreportView";
+
+    // Create an input field for the projectName parameter
+    var input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "projectName";
+    input.value = projectName;
+	form.appendChild(input);
+
+    // Append the form to the document body and submit it
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+  // Function to get a specific URL parameter by name
+  function getUrlParameter(name) {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  }
+
+  // Call the function when the page loads
+  window.onload = sendParametersToIframe;
+</script>
+
+<center>
+<table class="tlbFrame" width="100%" height="100%" border="0">
+<tr>
+  <td valign="top" align="center" width="400">
+    <iframe name="lreportView" src="lreports.php" width="100%" height="100%"></iframe>
+  </td>
+  <td>
+    <iframe name="reportView" src="SecSciAutoPT.htm" width="100%" height="100%"></iframe>
+  </td>
+</tr>
+</table>
+</center>
+</body>
+</html>
---
+++ Reports/lreports.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+    <meta name="referrer" content="no-referrer">
+    <title>Sec-Sci AutoPT Reports</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            font-family: Arial;
+            font-size: 12px
+        }
+
+        th, td {
+            padding: 3px;
+        }
+
+        body {
+            font-family: Arial;
+            color: #404042;
+            background: #dedede;
+        }
+
+        .frmContainer label {
+            font-size: 12px;
+            font-weight: 500;
+        }
+
+        a:link, a:visited {
+            text-decoration: none;
+            transform: 0.3s;
+        }
+
+        a:hover, a:active {
+            color: #e24920;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<center>
+    <table style="border: 0px;">
+        <tr>
+            <td style="border: 0px;" valign="top"><img src="SecSciAutoPT_icon.png" width="25" height="25"></td>
+            <td style="border: 0px;" valign="center"> <b style="font-family: Arial; font-size: 18px;">Sec-Sci AutoPT Reports</b></td>
+        </tr>
+    </table>
+    <form action="lreports.php" method="post">
+        <div class="frmContainer">
+            <label>Project Name:</label>
+            <input type="text" style="width: 120px;" name="projectName" value="" maxlength="18"/>
+            <input type="checkbox" name="chkArchive" value="Archived/"><label>Archived</label>
+            <input type="submit" name="submit" value="Search"/>
+        </div>
+    </form>
+    <?php
+    $chkArchive = isset($_POST["chkArchive"]) ? $_POST["chkArchive"] : "";
+    $projectName = isset($_POST["projectName"]) ? $_POST["projectName"] : "";
+    $webRootPath = $_SERVER["DOCUMENT_ROOT"];
+
+    if ($projectName === null) {
+        $projectName = "";
+    }
+    $pattern = "/[\/:*?\"<>|]/";
+    $projectName = preg_replace($pattern, "-", $projectName);
+    $projectName = str_replace('\\', '-', $projectName);
+
+    if ($chkArchive === "Archived/") {
+        $df = scandir($webRootPath . "/Archived");
+        echo "<b>Archived Sec-Sci AutoPT Reports</b><br><br>";
+    } else {
+        $df = scandir($webRootPath);
+    }
+	
+	$df = array_diff($df, array('.', '..'));
+    ?>
+    <table class="tblList">
+        <tr>
+            <td><b>Project Name</b></td>
+            <td><b>Burp File</b></td>
+            <td width="130"><b>Date & Time</b></td>
+        </tr>
+        <?php
+		
+		$fi = preg_grep("/$projectName.*\.burp$/i", $df);
+
+        foreach ($fi as $f) {
+            $fn = explode("_", $f);
+
+            echo "<tr>";
+            echo "<td><a href=\"" . $chkArchive . $f . ".html\" target=\"reportView\">" . $fn[0] . "</a></td>";
+            echo "<td><a href=\"" . $chkArchive . $f . "\" target=\"_blank\">Download</a></td>";
+            echo "<td>" . date("Y-m-d H:i:s", filemtime($webRootPath . "/" . $chkArchive . "/" . $f)) . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</center>
+</body>
+</html>
